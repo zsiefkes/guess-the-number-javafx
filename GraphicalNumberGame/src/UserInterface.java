8b9d14528b1efd8b9277c12646547f21787fcd52
Zachary Siefkes <zacharysiefkes@gmail.com>
2020-09-20 15:46:14 +1200

    Initial commit

---
+++ GraphicalNumberGame/src/UserInterface.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
